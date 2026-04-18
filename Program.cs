bef8aba27bdd0dc21504942ebd68a808fd657e64
Daniil Ternovyi <daniil.ternovyi@nure.ua>
2026-04-18 15:58:30 +0200

    Project initialization

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AgentForge orchestrator API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AgentForge orchestrator API v1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
